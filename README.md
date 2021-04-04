c9c3f5decf6055ae4ef1e4f368648d5a9767ceeb
Gustaf Gröning <gustafgroning@gmail.com>
2021-04-04 19:25:05 +0200

    finished first test, updated README

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# python__mashup_API
+## python__mashup_API
 
 
-# 
-I couldn't find any MBID that linked directly too Wikipedias API, and therefore the case where such a link appears hasn't been written. I have written a (likely) correct solution for further testing however (beneath elif entry['type'] == 'wikipedia': in the StartUp method)
+# Intro
+A RESTful API that gathers and formats data from 3 separate API’s (MusicBrainz, Wikidata/Wikipedia and Cover Art Archives) about an artist and their releases.
+
+Searches are based on a MBID (identifier from MusicBrainz API), which is used to gather and output:
+* ID (from MusicBrainz)
+* Description for the artist/band (from Wikipedia)
+* All releases from the artist/band with name and link to the front of the release (from Cover Art Archives).
+
+The output is in a JSON format.
+
+# Installation
+
+
+# Testing
+
+# Issues
+MusicBrainz can link directly from Wikipedia instead of going through WikiData, but I couldn’t find any ID’s linking directly, so I couldn’t test my implementation. Based on the format of MusicBrainz output a hypothetical solution is included in the source codes comments which should work for implementation, but hasn’t been tested properly.
--- app.py
+++ app.py
@@ -2,47 +2,36 @@ from flask import Flask, jsonify
 import requests
 import json
 
-
 app = Flask(__name__)
 
 
-mashUpDict = {"MBID": "", "description": "", "albums": ""} 
-#TODO: är alla variabler namngedda korrekt?
+mashUpDict = {"MBID": "", "description": "", "albums": ""} #dictionary där samtlig data sparas och till slut returneras.
 
-def findWikipediaURL(IDConvert, urlRework):
+def FindWikipediaURL(IDConvert, URLRework): #hittar via WikiData rätt ID för en sökning på Wikipedia.
 
     wikiDataURL = ("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" + IDConvert + "&format_=json&props=sitelinks&format=json")
     response = requests.get(wikiDataURL)
     data = response.text 
     parsed = json.loads(data) 
 
-    wiki_ID = parsed['entities'][IDConvert]['sitelinks']['enwiki']['title']
+    wikipediaID = parsed['entities'][IDConvert]['sitelinks']['enwiki']['title']
     
-    return(GetWikipediaText(wiki_ID.replace(" ", "_"), urlRework))
+    return(GetWikipediaText(wikipediaID.replace(" ", "_"), URLRework))
 
-def GetWikipediaText(wiki_ID, urlRework):
+def GetWikipediaText(wikipediaID, URLRework): #hämtar description text från Wikipedia.
 
-    url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + wiki_ID + "&formatversion=2&rvprop=content&rvslots=*&prop=extracts"
-    
-    response = requests.get(url) #hämter hela api:t
+    wikipediaURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + wikipediaID + "&formatversion=2&rvprop=content&rvslots=*&prop=extracts"
+    response = requests.get(wikipediaURL) 
+    data = response.text 
+    parsed = json.loads(data) 
 
-    data = response.text #gör om responsen till ett läsbart format
+    mashUpDict['description'] = (parsed['query']['pages'][0]['extract']) 
+    return(GetCoverArt(URLRework))
 
-    parsed = json.loads(data) # gör om json string till ett objekt
+def GetCoverArt(URLRework):
+    gatheredAlbums = {} #dictionary som sparar datan till slutreturningen (mashUpDict)
 
-    #print(parsed['query']['pages'][0]['extract'][0:1000]) 
-    mashUpDict['description'] = (parsed['query']['pages'][0]['extract'][0:1000]) #det ska inte ligga någon 0:1000 slice i slutversionen, men det gör det lättare att läsa output just nu.
-
-    #return(mashUpDict)
-
-    return(GetCoverArt(urlRework))
-
-def GetCoverArt(brainUrl):
-    gatheredAlbums = {}
-    
-    response = requests.get(brainUrl) #just nu bärs brainUrl genom hela grejen, det är inte en snygg lösning. Finns det någon bättre?
-    #coverUrl = "http://ia803208.us.archive.org/11/items/mbid-" + a146429a-cedc-3ab0-9e41-1aaf5f6cdc2d + "/index.json"
-    
+    response = requests.get(URLRework) 
     data = response.text
     parsed = json.loads(data)
 
@@ -52,49 +41,43 @@ def GetCoverArt(brainUrl):
         coverTitle = entry['title']
         coverID = entry['id']
         coverUrl = "http://coverartarchive.org/release-group/" + coverID 
-        # coverartarchive.org/release-group/810068af-2b3c-3e9c-b2ab-68a3f3e3787d det här formatet vill vi ha
         coverResponse = requests.get(coverUrl)
 
-        if coverResponse.status_code == 200:
+        if coverResponse.status_code == 200: #vissa albums fungerade inte, kanske felaktiga länkar från MusicBrainz. if-satsen fångar dessa 
+            #och hoppar över trasiga länkar.
             coverdata = coverResponse.text 
             coverParsed = json.loads(coverdata)
 
             imageLink = coverParsed['images'][0]['image']
 
-            
-            
             coverDictionary['image'] = imageLink
             coverDictionary['id'] = coverID
             coverDictionary['title'] = coverTitle
-            gatheredAlbums[coverTitle] = coverDictionary
+
+            gatheredAlbums[coverTitle] = coverDictionary #skapar hela albumet.
       
     
     mashUpDict['albums'] = gatheredAlbums
     finalOutput = json.loads(json.dumps(mashUpDict, indent = 4))
-    return(finalOutput)
+    return(finalOutput) #gör om slutlig output till JSON format.
     
-
-
-
 @app.route('/')
-def hi():
-    return("hello user!")
+def intro():
+    instructions = "to run the script, go too localhost/search/<>, with your requsted MBID inside < >"
+    return(instructions)
 
 @app.route('/search/<MBID>', methods=['GET'])
 
-def startUp(MBID): #tar ett MBID från användaren och hittar ett ID som skickas vidare till WikiData eller Wikipedia.
-# MBID ATT TESTA MED (nirvana) - 5b11f4ce-a62d-471e-81fc-a69a8278c7da
+def StartUp(MBID): #tar ett MBID från användaren och hittar ett ID som skickas vidare till WikiData eller Wikipedia.
     musicBrainzURL = "http://musicbrainz.org/ws/2/artist/" + MBID + "?&f%20mt=json&inc=url-rels+release-groups&fmt=json"
-    urlRework = ((musicBrainzURL.replace('<', '').replace('>', '')))
-    response = requests.get(urlRework) 
+    URLRework = ((musicBrainzURL.replace('<', '').replace('>', '')))
+    response = requests.get(URLRework) 
 
-    if response.status_code != 200:
-        status = str(response.status_code)
-        return("invalid MBID, status code: " + status)
-
+    if response.status_code != 200: #avslutar programmet ifall det inmatade MBID inte är korrekt.
+        errorStatus = str(response.status_code)
+        return("invalid MBID, status code: " + errorStatus)
 
     mashUpDict['MBID'] = (MBID)
-    
     data = response.text 
     parsed = json.loads(data) 
 
@@ -102,15 +85,15 @@ def startUp(MBID): #tar ett MBID från användaren och hittar ett ID som skickas
         
         if entry['type'] == 'wikidata':
             IDConvert = entry['url']['resource']
-            return(findWikipediaURL(IDConvert.split("/")[-1], urlRework)) #urlRework skickas med för att kunna hitta cover art senare.
+            return(FindWikipediaURL(IDConvert.split("/")[-1], URLRework)) #URLRework skickas med för att kunna hitta cover art senare, som behöver 
+            # komma åt samma URL. Sista splitten av IDConver (-1) innehåller information till Wikidatas API.
             
         elif entry['type'] == 'wikipedia':
             pass
         # Hittade inget ID som länkade direkt till Wikipedia, kunde därför inte testa detta case. 
-        # baserat på musicBrainz format bör lösningen dock vara;
-        # Wiki_ID = entry['title] - som sedan skickas direkt till GetWikipediaText.
+        # baserat på musicBrainz format bör dock lösningen vara;
+        # wikipediaID = entry['title] - som sedan skickas direkt till GetWikipediaText, utan att gå igenom FindWikipediaURL.
             
-
-
+# MBID ATT TESTA MED (nirvana) - 5b11f4ce-a62d-471e-81fc-a69a8278c7da
 if __name__ == '__main__':
     app.run(debug=True)
